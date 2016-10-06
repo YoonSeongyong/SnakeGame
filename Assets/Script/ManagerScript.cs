0245264270f6396168962f648192bc91dd6bf9d9
Seongyong Yoon <worrior321@gmail.com>
2016-10-06 18:33:22 +0900

    두번째 세팅
    배경, 스네이크 등?

---
+++ Assets/Script/ManagerScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ManagerScript : MonoBehaviour
+{
+    public GameObject snakeObj;
+    GameObject snake_h;
+    float scl, xspeed, yspeed;
+
+    // Use this for initialization
+    void Start()
+    {
+        snakeObj.transform.position = new Vector2(0, 0);
+        scl = 0.125f;
+        Debug.Log(Camera.main.pixelWidth);
+        //xspeed = Camera.main.pixelWidth / scl;
+        //yspeed = Camera.main.pixelHeight / scl;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("up"))
+        {
+
+        }
+    }
+    void MoveSnake()
+    {
+        snakeObj.transform.position = new Vector2(Mathf.Clamp(snakeObj.transform.position.x, -4.86f, 4.86f), Mathf.Clamp(snakeObj.transform.position.y, -2.68f, 2.68f));
+        snakeObj.transform.Translate(new Vector3(scl, 0, 0));
+
+    }
+}
