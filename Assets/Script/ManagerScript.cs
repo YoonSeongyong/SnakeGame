c1de05c202c7e7890b33e40b45fb3ab35984c0c1
Seongyong Yoon <worrior321@gmail.com>
2016-10-14 16:02:12 +0900

    third commit

--- Assets/Script/ManagerScript.cs
+++ Assets/Script/ManagerScript.cs
@@ -6,10 +6,11 @@ public class ManagerScript : MonoBehaviour
     public GameObject snakeObj;
     GameObject snake_h;
     float scl, xspeed, yspeed;
-
+    public Vector2 camWidth, camHeight; //x = min, y = max
     // Use this for initialization
     void Start()
     {
+        
         snakeObj.transform.position = new Vector2(0, 0);
         scl = 0.125f;
         Debug.Log(Camera.main.pixelWidth);
@@ -26,7 +27,7 @@ public class ManagerScript : MonoBehaviour
         }
     }
     void MoveSnake()
-    {
+    {   
         snakeObj.transform.position = new Vector2(Mathf.Clamp(snakeObj.transform.position.x, -4.86f, 4.86f), Mathf.Clamp(snakeObj.transform.position.y, -2.68f, 2.68f));
         snakeObj.transform.Translate(new Vector3(scl, 0, 0));
 
