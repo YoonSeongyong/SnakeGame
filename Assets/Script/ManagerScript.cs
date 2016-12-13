bb89fbd3b6d96f998e97010e39c4e50f6726b86e
YoonSeongyong <worrior321@gmail.com>
2016-12-13 12:53:56 +0900

    완성

--- Assets/Script/ManagerScript.cs
+++ Assets/Script/ManagerScript.cs
@@ -3,69 +3,38 @@ using System.Collections;
 using System.Collections.Generic;
 public class ManagerScript : MonoBehaviour
 {
-    public GameObject snakeObj;
-    //	private Camera cam = Camera.main;
-    List<GameObject> snakeBody = new List<GameObject>();
-    GameObject feed;
-    Vector2 camWidth = new Vector2(-11.5f, 11.5f);//x is min, y is max
-    Vector2 camHeight = new Vector2(-5.5f, 5.5f);
-    Vector2 dir = new Vector2(1, 0);
-    Vector2 snakePos;
+    public GameObject foodPrefab;
 
-    public string snake_name;
+	Vector2 camWidth = new Vector2(-11.5f, 11.5f);//x is min, y is max
+	Vector2 camHeight = new Vector2(-5.5f, 5.5f);
+	//	private Camera cam = Camera.main;
+    GameObject food;
+    Vector2 dir = new Vector2(1, 0);
+    Vector2 snakePos, prevPos;
+		
     // Use this for initialization
-    void Awake()
-    {
-        QualitySettings.vSyncCount = 0;
-        Application.targetFrameRate = 15;
-    }
+//    void Awake()
+//    {
+//        QualitySettings.vSyncCount = 0;
+//        Application.targetFrameRate = 15;
+//    }
 
     void Start()
     {
-        snakeObj.transform.position = new Vector2(0.5f, 0.5f);
-        snakeBody.Add(snakeObj);
-        snakePos = snakeObj.transform.position;
-        SpawnFeed();
+//        snakeObj.transform.position = new Vector2(0.5f, 0.5f);
+//        snakePos = snakeObj.transform.position;
+		InvokeRepeating("Spawn",3.0f,3.0f);
+    }		
+
+    void Spawn()
+    {
+		int x = Random.Range (-11,11);
+		int y = Random.Range (-5,5);
+		Instantiate (foodPrefab,new Vector2((float)x+0.5f,(float)y+0.5f),Quaternion.identity);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-            dir = new Vector2(0, 1);
-        else if (Input.GetKeyDown(KeyCode.DownArrow))
-            dir = new Vector2(0, -1);
-        else if (Input.GetKeyDown(KeyCode.LeftArrow))
-            dir = new Vector2(-1, 0);
-        else if (Input.GetKeyDown(KeyCode.RightArrow))
-            dir = new Vector2(1, 0);
-        MoveSnake();
-    }
-
-
-    void MoveSnake()
-    {
-        snakePos.x += dir.x;
-        snakePos.y += dir.y;
-        //snakePos.x += dir.x*0.1f;
-        //snakePos.y += dir.y*0.1f;
-        snakePos = new Vector2(Mathf.Clamp(snakePos.x, camWidth.x, camWidth.y), Mathf.Clamp(snakePos.y, camHeight.x, camHeight.y));
-        snakeObj.transform.position = snakePos;
-        //snakeObj.transform.Translate(new Vector3(scl, 0, 0));
-
-    }
-
-    void SpawnFeed()
-    {
-        Vector2 snake_b = new Vector2((float)RandPos(-11, 11) + 0.5f, (float)RandPos(-5, 5) + 0.5f);
-        feed = Instantiate(snakeObj, snake_b, Quaternion.identity) as GameObject;
-    }
-
-    void Eat()
-    {
-        SpawnFeed();
-        //snakeBody.Add(feed);
-        //snakeBody.FindLast().name = snake_name + "1";
+    public void Eat()
+	{
 
     }
 
--- Assets/Script/SnakeScript.cs
+++ Assets/Script/SnakeScript.cs
@@ -1,20 +1,64 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 public class SnakeScript : MonoBehaviour {
-
+	public ManagerScript mgr;
+	public GameObject tailPrefab;
+	public GameObject gameOverTxt;
+	Vector2 dir = Vector2.right;
+	Vector2 camWidth = new Vector2(-11.5f, 11.5f);//x is min, y is max
+	Vector2 camHeight = new Vector2(-5.5f, 5.5f);
+	bool ate = false;
+	List<Transform> tail = new List<Transform>();
 	// Use this for initialization
 	void Start () {
-	
+		InvokeRepeating ("Move", 0.3f, 0.05f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
 
-    void OnCollisionEnter2D(Collision2D col)
-    {
-        //ManagerScript.
-    }
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+			dir = new Vector2(0, 1);
+		else if (Input.GetKeyDown(KeyCode.DownArrow))
+			dir = new Vector2(0, -1);
+		else if (Input.GetKeyDown(KeyCode.LeftArrow))
+			dir = new Vector2(-1, 0);
+		else if (Input.GetKeyDown(KeyCode.RightArrow))
+			dir = new Vector2(1, 0);
+	}
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.name.StartsWith ("foodPrefab")) {
+			ate = true;
+
+			Destroy (col.gameObject);
+		} else {
+			Time.timeScale = 0f;
+			CancelInvoke ();
+			gameOverTxt.SetActive (true);
+		}
+	}
+	void Eat()
+	{
+		
+	}
+	void Move()
+	{
+		Vector2 curPos = transform.position;
+		transform.Translate (dir);
+//		transform.position = new Vector2(Mathf.Clamp(transform.position.x, camWidth.x, camWidth.y), Mathf.Clamp(transform.position.y, camHeight.x, camHeight.y));
+		if(ate){
+			GameObject _tail = (GameObject)Instantiate (tailPrefab,curPos,Quaternion.identity);
+			tail.Insert (0,_tail.transform);
+			ate = false;
+		}else if(tail.Count > 0){
+			tail.Last ().position = curPos;
+
+			tail.Insert (0, tail.Last());
+			tail.RemoveAt (tail.Count-1);
+		}
+	}
 }
